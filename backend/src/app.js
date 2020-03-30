3734c97f4a09e2404a50ba8d319435eed1e8aaad
Chrigor <chrigor.edu3@gmail.com>
2020-03-30 15:22:26 -0300

    adicionando testes no nosso backend, tanto test unitario quanto de integração

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const {errors} = require("celebrate")
 const routes = require("./routes");
 
 app.use(cors()); // liberado para todas
 app.use(express.json()); // aceite json e o transforme em object 
 app.use(routes);
+app.use(errors());
 
 
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log(`O servidor está escutando na porta ${port}`);
-});
+module.exports = app;
